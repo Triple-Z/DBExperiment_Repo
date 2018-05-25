2d12920d469e0f543a21971916451f3f111bace5
TripleZ <me@triplez.cn>
2018-05-25 20:33:43 +0800

    Finish tasks in experiment 2

--- exp_1_2.sql
+++ exp_1_2.sql
@@ -110,4 +110,57 @@ where not exists(
   )
 );
 
+# 2-5
+# 查询所有学生除了选修 1 号课程外所有成绩均及格的学生的学号和平均成绩,其结果按平均成绩的降序排列
+select SCX.Sno, avg(SCX.Grade)
+from SC SCX
+where 59 < all(
+  select SCY.Grade
+  from SC SCY
+  where SCY.Sno=SCX.Sno
+    and SCY.Cno!='1'
+) and SCX.Cno != '1'
+group by SCX.Sno
+order by avg(SCX.Grade) desc;
+
+# 2-6
+# 查询选修数据库原理成绩第 2 名的学生姓名。
+select S.Sname
+from SC, C, S
+where C.Cname='数据库原理'
+  and SC.Sno=S.Sno
+  and SC.Cno=C.Cno
+group by SC.Sno
+order by SC.Grade desc
+limit 1, 1;
+
+# 2-7
+# 查询所有 3 个学分课程中有 3 门以上(含 3 门)课程获 80 分以上(含 80 分)的学生的姓名。
+select Sname
+from S
+where Sno in(
+  select Sno
+  from SC, C
+  where C.Cno=SC.Cno
+    and C.Ccredit=3
+    and SC.Grade >= 80
+  group by Sno
+  having count(Grade) >= 3
+);
+
+# 2-8
+# 查询选课门数唯一的学生的学号。
+select Sno, count(Cno)
+from SC
+group by Sno;
+
+select distinct SC.Sno
+from SC, (select Sno, count(Cno) Count_CNO from SC group by Sno) as Count_SC
+where not exists(
+    select * from (select Sno, count(Cno) Count_CNO from SC group by Sno) as Count_SC_2
+    where SC.Sno != Count_SC_2.Sno
+      and Count_SC.Count_CNO=Count_SC_2.Count_CNO
+  )
+  and SC.Sno=Count_SC.Sno;
+
 
